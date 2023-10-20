43fc7d8ffb392664f889ffbd6c13346f1be5ab73
omelenko <vasul.melenko@gmail.com>
2023-10-20 18:04:07 +0300

    Add project files.

---
+++ ConsoleApp6/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Threading;
+
+class Program
+{
+    static int freq = 200, dura = 1000;
+    static void Main()
+    {
+        Thread thread = new Thread(new ThreadStart(() => Worker()));
+        Thread thread1 = new Thread(new ThreadStart(() => Worker1()));
+        thread.Start();
+        thread1.Start();
+        thread.Join();
+        thread1.Join();
+    }
+
+    static void Worker()
+    {
+        for (int i = 0; i < 100; i++)
+        {
+            generateSoundAndSleep(freq, dura, 500);
+            freq += 18;
+        }
+    }
+    static void Worker1()
+    {
+        for (int i = 0; i < 100; i++)
+        {
+            Console.WriteLine($"Частота: {freq}");
+            Thread.Sleep(1500);
+        }
+    }
+
+    static void generateSoundAndSleep(int freq, int dura, int sleep)
+    {
+        Console.Beep(freq, dura);
+        Thread.Sleep(sleep);
+    }
+}
